cb38eb6ad8513f1a523bbd7cc892924fb7876aaf
WeiTheShinobi <weitheshinobi@gmail.com>
2024-06-29 23:17:08 +0800

    add parse function

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,30 @@ fn main() -> io::Result<()> {
     let mut file = File::open(FILE_NAME)?;
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)?;
-    println!("File contents as bytes: {:?}", buffer);
 
     Ok(())
 }
 
+fn parse_msg_len(v: &[u8]) -> usize {
+    (parse_bcd(v[0]) as usize) * 100 + parse_bcd(v[1]) as usize
+}
+
+fn parse_bcd(v: u8) -> u8 {
+    10 * (v >> 4) + (v & 0b00001111)
+}
+
+#[test]
+fn test_parse_bcd() {
+    assert_eq!(parse_bcd(0b00010010), 12);
+    assert_eq!(parse_bcd(0b10010011), 93);
+    assert_eq!(parse_bcd(0b00000001), 1);
+    assert_eq!(parse_bcd(0b00010000), 10);
+}
+
+#[test]
+fn test_parse_msg_len() {
+    assert_eq!(parse_msg_len(&[0b00010010, 0b10010011]), 1293);
+    assert_eq!(parse_msg_len(&[0b00000001, 0b00100010]), 122);
+    assert_eq!(parse_msg_len(&[0b00010000, 0b00000001]), 1001);
+    assert_eq!(parse_msg_len(&[0b00000000, 0b00000001]), 1);
+}
