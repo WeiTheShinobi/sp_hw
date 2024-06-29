a0ce6d2abcbc4f8c002671204c4f713095ab0cd4
WeiTheShinobi <weitheshinobi@gmail.com>
2024-06-29 20:04:45 +0800

    load file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+const FILE_NAME: &str = "f6_01000001_01001000_TP03.new";
+
+fn main() -> io::Result<()> {
+    let mut file = File::open(FILE_NAME)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    println!("File contents as bytes: {:?}", buffer);
+
+    Ok(())
 }
+
